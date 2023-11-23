5b1118dcca50ffdaca9649817bc0534b601cf00e
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2023-11-23 09:44:39 +0000

    restore backticks to queryString queries

--- models/app.models.js
+++ models/app.models.js
@@ -12,10 +12,10 @@ exports.selectTopics = () => {
 }
 
 exports.selectArticles = (topic) => {
-    let queryString = 'SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, '
-    queryString += 'COUNT(comments.article_id) as comment_count '
-    queryString += 'FROM articles '
-    queryString += 'LEFT JOIN comments ON articles.article_id = comments.article_id '
+    let queryString = `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, 
+    COUNT(comments.article_id) as comment_count 
+    FROM articles 
+    LEFT JOIN comments ON articles.article_id = comments.article_id `
     const queryValues = []
 
     if(topic) {
@@ -30,8 +30,9 @@ exports.selectArticles = (topic) => {
 }
 
 exports.selectArticleById = (id) => {
-    let queryString = 'SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url, '
-    queryString += 'CAST(COUNT(comments.article_id) AS INT) as comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id '
+    let queryString = `SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url, 
+    CAST(COUNT(comments.article_id) AS INT) as comment_count FROM articles 
+    LEFT JOIN comments ON articles.article_id = comments.article_id `
     const queryValues = []
 
     queryString += 'WHERE articles.article_id = $1 '
